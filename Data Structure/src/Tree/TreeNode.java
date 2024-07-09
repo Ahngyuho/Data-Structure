88d2b87a16ded56a6c331a92479c87e468327cd5
ahn <gyuho0897@gmail.com>
2024-07-09 21:37:29 +0900

    Add #1 TreeNode

---
+++ Data Structure/src/Tree/TreeNode.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+
+}
